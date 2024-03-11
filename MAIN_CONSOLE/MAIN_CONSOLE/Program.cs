ba090f6ea1a716f21cb776165716661e36acad1a
K1tsune <60034144+Kitsune424@users.noreply.github.com>
2024-03-11 11:26:27 +0300

    "Начало пути" Созданы великолепные MAIN_CONSOLE и MAIN_LIBRARY Пожалуйста, не создавайте тут ничего больше, чем есть сейчас Initial commit

---
+++ MAIN_CONSOLE/MAIN_CONSOLE/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MAIN_LIBRARY;
+
+namespace MAIN_CONSOLE
+{
+    /// <summary>
+    /// ЭТО MAIN_CONSOLE
+    /// ТУТ НУЖНО УЖЕ ПОДКЛЮЧЕНА БИБЛИОТЕКА В КОТОРОЙ СОЗДАНЫ ВАШИ КЛАССЫ
+    /// ЭТОТ ФАЙЛ ДОЛЖЕН БЫТЬ ОДИН ЕДИНСТВЕННЫЙ. ЕСЛИ ИХ СТАЛО 2 И БОЛЕЕ
+    /// ТО ВЫ БУБЫЛДА ДУРАЦКАЯ И ВСЕ СЛОМАЛИ
+    /// </summary>
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
